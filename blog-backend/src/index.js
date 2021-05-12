1ddadd79b07ff8db2efcc2308064ec69f2139258
Kyubum Jang <jkb2221@gmail.com>
2021-05-12 17:16:09 +0900

    Initial commit

---
+++ blog-backend/src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+// 비구조화 할당을 통해 porcess.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env;
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+//라우터 설정
+router.use('/api', api.routes()); //api 라우트 적용
+
+//라우터 적용 전에 body parser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log('Listening to port %d', port);
+});
