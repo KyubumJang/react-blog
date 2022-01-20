0bd2b87336a105c78d9393b26f2961996e6eb09f
Kyubum Jang <jkb2221@gmail.com>
2022-01-20 17:48:09 +0900

    refactor: import/export으로 require문 수정
    - esm 라이브러리 사용하여 import export 구문 사용하는 것으로 코드 변경

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,26 +1,4 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-
-// 비구조화 할당을 통해 porcess.env 내부 값에 대한 레퍼런스 만들기
-const { PORT } = process.env;
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-//라우터 설정
-router.use('/api', api.routes()); //api 라우트 적용
-
-//라우터 적용 전에 body parser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-const port = PORT || 4000;
-app.listen(port, () => {
-    console.log('Listening to port %d', port);
-});
+// 이 파일에서만 no-glbal-assign ESLint 옵션 비활성화
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module /*, options */);
+module.exports = require('./main.js');
