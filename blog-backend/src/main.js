1e31c1d9ab8f4a70225dd4c5e5f95b0a6ff11436
Kyubum Jang <jkb2221@gmail.com>
2022-01-28 18:25:17 +0900

    feat: add post fake data

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
-import Koa from 'koa'
+import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+// import createFakeData from './createFakeData';
 
 // 비구조화 할당을 통해 porcess.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-.connect(MONGO_URI, {useNewUrlParser: true })
-.then(()=> {
-    console.log('Connected to MongoDB');
-})
-.catch(e => {
-    console.error(e);
-})
+    .connect(MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // createFakeData();
+    })
+    .catch((e) => {
+        console.error(e);
+    });
 
 const api = require('./api');
 
@@ -31,6 +33,6 @@ app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
-app.listen(port, () => { 
+app.listen(port, () => {
     console.log('Listening to port %d', port);
 });
