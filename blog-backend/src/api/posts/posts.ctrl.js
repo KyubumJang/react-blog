3b9ea95f52c2cbabf9d2d7bfb7b6f9df8052ae41
Kyubum Jang <jkb2221@gmail.com>
2022-01-28 18:13:30 +0900

    feat: objectId, Request Body 검증

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,33 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi'
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400; //Bad request
+        return;
+    }
+    return next();
+}
 
 export const write = async ctx => {
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증
+        title: Joi.string().required(), // required()가 있을 시 필수
+        body: Joi.string().required(), 
+        tags: Joi.array().items(Joi.string()).required(),
+    })
+    //검증 후 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; //Bad Request
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post({ title, body, tags });
     try {
@@ -10,6 +37,7 @@ export const write = async ctx => {
         ctx.throw(500, e)
     }
 };
+
 export const list = async ctx => {
     try {
         const posts = await Post.find().exec();
@@ -18,6 +46,7 @@ export const list = async ctx => {
         ctx.throw(500, e)
     }
 };
+
 export const read = async ctx => {
     const { id } = ctx.params;
     try {
@@ -31,6 +60,7 @@ export const read = async ctx => {
         ctx.throw(500,e)
     }
 };
+
 export const remove = async ctx => {
     const {id} = ctx.params;
     try {
@@ -40,8 +70,21 @@ export const remove = async ctx => {
         ctx.throw(500, e)
     }
 };
+
 export const update = async ctx => {
     const {id} = ctx.params;
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    })
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; //Bad Request
+        ctx.body = result.error;
+        return;
+    }
+    
     try {
         //이 값을 설정하면 업데이트 된 데이터 반환, false일 경우 업데이트 되기 전 데이터 반환)
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, { new: true }).exec();
